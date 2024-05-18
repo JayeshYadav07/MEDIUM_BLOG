ceaae9fea2ee772cede38b6055d2a21e2154d72f
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-05-18 13:00:18 +0530

    signup and login routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,89 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { sign, verify } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        SECRET_KEY: string;
+    };
+}>();
 
 app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
 
-// POST /api/v1/user/signup
-app.post("/app/v1/user/signup", (c) => {
-    return c.text("POST /api/v1/user/signup");
+const signupSchema = z.object({
+    email: z.string(),
+    password: z.string(),
+    name: z.string().optional(),
 });
 
-// POST /api/v1/user/signin
-app.post("/app/v1/user/signin", (c) => {
-    return c.text("POST /api/v1/user/signin");
+// Create User
+app.post("/app/v1/user/signup", zValidator("json", signupSchema), async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password: body.password,
+                name: body.name,
+            },
+        });
+        return c.json({
+            msg: "User created successfully!",
+            user,
+        });
+    } catch (error) {
+        return c.json({
+            msg: "Something went wrong!",
+        });
+    }
+});
+
+const loginSchema = z.object({
+    email: z.string(),
+    password: z.string(),
+});
+
+// Login User
+app.post("/app/v1/user/signin", zValidator("json", loginSchema), async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email: body.email,
+                password: body.password,
+            },
+        });
+        if (!user) {
+            return c.json({
+                msg: "Wrong Credentials!",
+            });
+        } else {
+            const token = await sign({ userId: user.id }, c.env.SECRET_KEY);
+            return c.json({
+                msg: "User Login successfully!",
+                user,
+                token: token,
+            });
+        }
+    } catch (error) {
+        return c.json({
+            msg: "Something went wrong!",
+        });
+    }
 });
 
 // POST /api/v1/blog
