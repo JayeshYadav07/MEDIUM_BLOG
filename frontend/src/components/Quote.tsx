1e912ba0bd66c029c17a7a8b6b1bf3466bc2861b
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-05-19 20:17:19 +0530

    Component : signup quote component created

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+function Quote() {
+    return (
+        <div className="h-screen bg-slate-200 flex flex-col justify-center items-center invisible lg:visible">
+            <div className="max-w-lg font-semibold leading-relaxed">
+                <div className="text-3xl text-left my-2">
+                    "Don’t try to tell the customer what he wants. If you want
+                    to be smart, be smart in the shower. Then get out, go to
+                    work and serve the customer!"
+                </div>
+                <div className="text-xl">Gene Buckley</div>
+                <div className="text-sm text-gray-500">CEO | Anna Segova</div>
+            </div>
+        </div>
+    );
+}
+
+export default Quote;
