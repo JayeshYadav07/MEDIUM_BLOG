a9a88818be35f71701b975e1952a1d4e296d9ede
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-05-21 09:56:16 +0530

    Added FE : toast added for better alert

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -3,6 +3,8 @@ import { ChangeEvent, useState } from "react";
 import { SignupType } from "jayeshyadav_medium_common";
 import { BACKEND_URL } from "../config/config";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Auth({ type }: { type: "signup" | "signin" }) {
     const navigate = useNavigate();
@@ -21,80 +23,91 @@ function Auth({ type }: { type: "signup" | "signin" }) {
                 formInputs
             );
             const data = res.data;
-            console.log(data);
-            if (!data.error) navigate("/blog");
-            else alert(data.error);
+            if (!data.error) {
+                toast.success(data.msg, { autoClose: 500 });
+                setTimeout(() => {
+                    navigate("/blog");
+                }, 1000);
+            } else {
+                toast.error(data.error, { autoClose: 3000 });
+            }
         } catch (error) {
-            console.log("Error while signing up");
+            toast.error(
+                `Error while ${type == "signup" ? "signup" : "signin"} `,
+                { autoClose: 3000 }
+            );
         }
     }
 
     return (
-        <div className=" h-screen flex flex-col justify-center items-center">
-            <div>
-                <div className="px-7 my-4 text-center">
-                    <h1 className="text-3xl font-bold">
-                        {type == "signup"
-                            ? "Create an account"
-                            : "Login an account"}
-                    </h1>
-                    <p className="text-base text-gray-400">
-                        {type == "signup"
-                            ? "Already have an account?"
-                            : "Don't have an account"}
-                        <Link
-                            className="text-blue-600 underline px-1"
-                            to={type == "signup" ? "/signin" : "/signup"}
-                        >
-                            {type == "signup" ? "Login" : "Sign Up"}
-                        </Link>
-                    </p>
-                </div>
+        <>
+            <ToastContainer />
+            <div className=" h-screen flex flex-col justify-center items-center">
                 <div>
-                    {type == "signup" && (
+                    <div className="px-7 my-4 text-center">
+                        <h1 className="text-3xl font-bold">
+                            {type == "signup"
+                                ? "Create an account"
+                                : "Login an account"}
+                        </h1>
+                        <p className="text-base text-gray-400">
+                            {type == "signup"
+                                ? "Already have an account?"
+                                : "Don't have an account"}
+                            <Link
+                                className="text-blue-600 underline px-1"
+                                to={type == "signup" ? "/signin" : "/signup"}
+                            >
+                                {type == "signup" ? "Login" : "Sign Up"}
+                            </Link>
+                        </p>
+                    </div>
+                    <div>
+                        {type == "signup" && (
+                            <LabelledInput
+                                label="Username"
+                                placeholder="Enter your name"
+                                onChange={(e) => {
+                                    setFormInputs((c) => ({
+                                        ...c,
+                                        name: e.target.value,
+                                    }));
+                                }}
+                            />
+                        )}
                         <LabelledInput
-                            label="Username"
-                            placeholder="Enter your name"
+                            label="Email"
+                            type="email"
+                            placeholder="Enter your Email"
                             onChange={(e) => {
                                 setFormInputs((c) => ({
                                     ...c,
-                                    name: e.target.value,
+                                    email: e.target.value,
                                 }));
                             }}
                         />
-                    )}
-                    <LabelledInput
-                        label="Email"
-                        type="email"
-                        placeholder="Enter your Email"
-                        onChange={(e) => {
-                            setFormInputs((c) => ({
-                                ...c,
-                                email: e.target.value,
-                            }));
-                        }}
-                    />
-                    <LabelledInput
-                        label="Password"
-                        type="password"
-                        placeholder="Enter your password"
-                        onChange={(e) => {
-                            setFormInputs((c) => ({
-                                ...c,
-                                password: e.target.value,
-                            }));
-                        }}
-                    />
+                        <LabelledInput
+                            label="Password"
+                            type="password"
+                            placeholder="Enter your password"
+                            onChange={(e) => {
+                                setFormInputs((c) => ({
+                                    ...c,
+                                    password: e.target.value,
+                                }));
+                            }}
+                        />
+                    </div>
+                    <button
+                        onClick={sendRequest}
+                        type="button"
+                        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 w-full"
+                    >
+                        {type == "signup" ? "Sign Up" : "Sign In"}
+                    </button>
                 </div>
-                <button
-                    onClick={sendRequest}
-                    type="button"
-                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 w-full"
-                >
-                    {type == "signup" ? "Sign Up" : "Sign In"}
-                </button>
             </div>
-        </div>
+        </>
     );
 }
 
