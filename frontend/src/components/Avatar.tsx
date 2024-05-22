4d3962cdcd3e41bbd334d448fa0d8d41141014ec
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-05-22 23:57:45 +0530

    Component FE : Avatar component added

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 function Avatar({ name }: { name: string }) {
     return (
-        <div className="bg-green-400 text-xl flex justify-center items-center h-10 w-10 rounded-full">
-            {name[0]}
+        <div className="bg-gray-300 text-xl flex justify-center items-center h-10 w-10 rounded-full">
+            {name[0].toUpperCase()}
         </div>
     );
 }
