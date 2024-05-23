898a35e1caa787a38e8a839f25bad45b9ee6433d
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-05-23 13:25:23 +0530

    Schema BE : migrate schema to set blog date

--- backend/src/blog.ts
+++ backend/src/blog.ts
@@ -89,6 +89,13 @@ blogRoutes.get("/:id", async (c) => {
             where: {
                 id: id,
             },
+            include: {
+                author: {
+                    select: {
+                        name: true,
+                    },
+                },
+            },
         });
         return c.json(getBlog);
     } catch (error) {
