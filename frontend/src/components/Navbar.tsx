37f6484d2e09135e6a1117ed26c18af28072d781
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-05-25 20:06:25 +0530

    styling fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,25 +18,37 @@ function Navbar() {
         }, 1000);
     };
     return (
-        <div className="flex justify-between items-center py-2 border-b px-2 md:px-4 lg:px-12">
+        <>
             <ToastContainer />
-            <Link className="cursor-pointer hover:text-blue-600" to={"/blog"}>
-                MEDIUM
-            </Link>
-            <div className="flex items-center">
-                <Link to={"/publish"}>
-                    <Button size="small" variant="contained" color="primary">
-                        Publish
-                    </Button>
+            <div className="flex justify-between items-center py-2 border-b px-2 md:px-4 lg:px-12">
+                <Link
+                    className="cursor-pointer hover:text-blue-600"
+                    to={"/blog"}
+                >
+                    MEDIUM
                 </Link>
-                <div className="px-4">
-                    <Avatar name={name} />
-                </div>
-                <div>
-                    <TbLogout className="text-2xl" onClick={performLogout} />
+                <div className="flex items-center">
+                    <Link to={"/publish"}>
+                        <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                        >
+                            Publish
+                        </Button>
+                    </Link>
+                    <div className="px-4">
+                        <Avatar name={name} />
+                    </div>
+                    <div>
+                        <TbLogout
+                            className="text-2xl"
+                            onClick={performLogout}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
